5bedf7e4d998208ebdfbcc300a1b18d333935fb6
egbai mmumene <mumene@gmail.com>
2019-02-22 12:40:22 +0300

    fix to enabling status code so void response can be handled

--- ketro/src/main/java/com/past3/ketro/api/Kobserver.kt
+++ ketro/src/main/java/com/past3/ketro/api/Kobserver.kt
@@ -1,23 +1,38 @@
 package com.past3.ketro.api
 
 import android.arch.lifecycle.Observer
+import com.past3.ketro.model.StatusCode
 import com.past3.ketro.model.Wrapper
 
-abstract class Kobserver<T> : Observer<Wrapper<T>> {
+/**
+ *  @constructor Kobserver takes in a function to handle exceptions, note when
+ *   you pass in a function the onException(exception:Exception) method will
+ *   be ignored
+ */
+abstract class Kobserver<T>(private val exceptionHandler: ((Exception) -> Unit)? = null) : Observer<Wrapper<T>> {
 
     override fun onChanged(wrapper: Wrapper<T>?) {
-        finish()
         wrapper?.let {
-            it.exception?.let {
-                onException(it)
+            finish(it.statusCode)
+            it.exception?.let { ex ->
+                handleErrorInstance(ex)
             }
-            it.data?.let {
-                onSuccess(it)
+            it.data?.let { data ->
+                onSuccess(data)
             }
             return
         }
-        onException(Exception("Something went wrong"))
+        handleErrorInstance(Exception("Something went wrong"))
     }
+
+    private fun handleErrorInstance(ex: Exception) {
+        exceptionHandler?.let { func ->
+            func.invoke(ex)
+            return
+        }
+        onException(ex)
+    }
+
     /**
      * Method to be called on successful load of data. This method is guaranteed to contain the returned data
      *
@@ -29,13 +44,16 @@ abstract class Kobserver<T> : Observer<Wrapper<T>> {
      * Method to handle exception that occurs during request processing.
      * @param exception
      */
-    abstract fun onException(exception: Exception)
+    open fun onException(exception: Exception) {
+
+    }
 
     /**
-     * Triggered when internet request is cancelled by user.
+     * Triggered for any request by user, both successful and unsuccessful
+     * takes in a StatusCode, so you can determine the state of the requests
      */
 
-    fun finish() {
-
+    open fun finish(code: StatusCode) {
+        //Check for 20x and perform operation
     }
 }
--- ketro/src/main/java/com/past3/ketro/model/Wrapper.kt
+++ ketro/src/main/java/com/past3/ketro/model/Wrapper.kt
@@ -1,6 +1,16 @@
 package com.past3.ketro.model
 
-class Wrapper<T> {
-    var exception: Exception? = null
-    var data: T? = null
-}
+/**
+ *    Wrapper for Api responses
+ *   @param exception Returns thrown exception or null if successful request
+ *   @param data Returns response object or null if empty object or not successful
+ *   @param statusCode Returns the request state or -1 if request does not happen due to internet
+ *   or similar connectivity issues
+ */
+class Wrapper<T>(
+        val exception: Exception? = null,
+        val data: T? = null,
+        val statusCode: StatusCode
+)
+
+inline class StatusCode(val code: Int)
