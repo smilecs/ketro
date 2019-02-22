1a9fd4b7574a9ae8cf3376dd512a48b8196e3cc9
Smile Egbai <mumene@gmail.com>
2019-02-22 13:57:58 +0300

    #11 (#13) (#14)
    * Set theme jekyll-theme-minimal
    
    * Mascot image
    
    * Add mascot image to readme.md
    
    * Update README.md
    
    made edits for better interpretation
    
    * Update README.md
    
    * changed sample project
    
    changed sample project to make it contain basic usage of the library's components
    
    * corrected logic for views visibility
    
    * optimized input text field to restrict input to single line
    
    * Update README.md
    
    * Update issue templates
    
    * Update issue templates
    
    * Update README.md
    
    * fix to enabling status code so void response can be handled

--- ketro/src/main/java/com/past3/ketro/api/ApiCallback.kt
+++ ketro/src/main/java/com/past3/ketro/api/ApiCallback.kt
@@ -1,28 +1,31 @@
 package com.past3.ketro.api
 
+import com.past3.ketro.model.StatusCode
 import retrofit2.Call
 import retrofit2.Callback
 
 abstract class ApiCallback<T>(private val errorHandler: ApiErrorHandler) : Callback<T> {
 
-    override fun onResponse(call: Call<T>, response: retrofit2.Response<T>) =
-            if (response.body() != null) {
-                handleResponseData(response.body()!!)
-            } else {
-                handleError(response)
-            }
-
-    protected abstract fun handleResponseData(data: T)
-
-    private fun handleError(response: retrofit2.Response<T>) {
-        handleException(errorHandler.getExceptionType(response))
+    override fun onResponse(call: Call<T>, response: retrofit2.Response<T>) {
+        val statusCode = StatusCode(response.code())
+        if (response.body() != null) {
+            handleResponseData(response.body()!!, statusCode)
+        } else {
+            handleError(response, statusCode)
+        }
     }
 
-    protected abstract fun handleException(t: Exception)
+    protected abstract fun handleResponseData(data: T, statusCode: StatusCode)
+
+    private fun handleError(response: retrofit2.Response<T>, statusCode: StatusCode) {
+        handleException(errorHandler.getExceptionType(response), statusCode)
+    }
+
+    protected abstract fun handleException(ex: Exception, statusCode: StatusCode)
 
     override fun onFailure(call: Call<T>, t: Throwable) {
         if (t is Exception) {
-            handleException(t)
+            handleException(t, StatusCode(-1))
         } else {
             //do something else
         }
