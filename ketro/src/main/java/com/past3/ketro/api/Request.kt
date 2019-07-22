59e22c8264a6b49cd319cb83ec843fec0603442f
Smile Egbai <mumene@gmail.com>
2019-07-22 12:38:12 +0300

    #10 (#23)
    * WIP: Proof of concept[example files will be deleted]
    
    * #10 added Coroutine support

---
+++ ketro/src/main/java/com/past3/ketro/api/Request.kt
@@ -0,0 +1,37 @@
+package com.past3.ketro.api
+
+import com.past3.ketro.model.StatusCode
+import com.past3.ketro.model.Wrapper
+import retrofit2.Response
+
+abstract class Request<T> {
+
+    var errorHandler: ApiErrorHandler = ApiErrorHandler()
+
+    protected abstract suspend fun makeRequest(): Response<T>
+
+
+    suspend fun doRequest(): Wrapper<T> {
+        return makeRequest().run {
+            val statusCode = StatusCode(code())
+            when (statusCode.code) {
+                in 200 until 209 -> {
+                    handleResponseData(body(), statusCode)
+                }
+                else -> {
+                    handleError(this, statusCode)
+                }
+            }
+
+        }
+    }
+
+    private fun handleResponseData(data: T?, statusCode: StatusCode): Wrapper<T> {
+        return Wrapper(data = data, statusCode = statusCode)
+    }
+
+    private fun handleError(response: Response<T>, statusCode: StatusCode): Wrapper<T> {
+        return Wrapper(errorHandler.getExceptionType(response), statusCode = statusCode)
+    }
+
+}
