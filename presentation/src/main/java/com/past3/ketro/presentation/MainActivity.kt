a7d6f6a5f94d0edbd799479680143c9da9704610
Smile Egbai <mumene@gmail.com>
2022-06-19 13:25:27 +0200

    Add build type (#51)
    * gradle build
    
    * gradle build
    
    * refactor

--- presentation/src/main/java/com/past3/ketro/presentation/MainActivity.kt
+++ presentation/src/main/java/com/past3/ketro/presentation/MainActivity.kt
@@ -5,10 +5,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.past3.ketro.domain.entities.ErrorConfig
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.main_activity.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity(), View.OnClickListener {
@@ -25,56 +22,56 @@ class MainActivity : DaggerAppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
-        recyclerView.let {
-            it.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
-            it.adapter = listAdapter
-            it.setHasFixedSize(true)
-        }
-
-        listAdapter.submitList(viewModel.list)
-        searchButton.setOnClickListener(this@MainActivity)
-
-        viewModel._liveData.observe(this, Observer {
-            toggleViews(true)
-            Toast.makeText(this@MainActivity, "Works", Toast.LENGTH_LONG).show()
-        })
-
-        viewModel.errorLiveData.observe(this, Observer { ex ->
-            ex?.let {
-                userErrorHanlder(it)
-            }
-        })
+//        recyclerView.let {
+//            it.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+//            it.adapter = listAdapter
+//            it.setHasFixedSize(true)
+//        }
+//
+//        listAdapter.submitList(viewModel.list)
+//        searchButton.setOnClickListener(this@MainActivity)
+//
+//        viewModel._liveData.observe(this, Observer {
+//            toggleViews(true)
+//            Toast.makeText(this@MainActivity, "Works", Toast.LENGTH_LONG).show()
+//        })
+//
+//        viewModel.errorLiveData.observe(this, Observer { ex ->
+//            ex?.let {
+//                userErrorHanlder(it)
+//            }
+//        })
 
     }
 
     private fun userErrorHanlder(ex: Exception) {
-        toggleViews(false)
-        when (ex) {
-            is ErrorConfig.NetworkException -> {
-                Toast.makeText(this@MainActivity, ex.message, Toast.LENGTH_LONG).show()
-            }
-            is ErrorConfig.GitHubException -> {
-                Toast.makeText(this@MainActivity, ex.message, Toast.LENGTH_LONG).show()
-            }
-            else -> Toast.makeText(this@MainActivity, "Oops! Something went wrong.", Toast.LENGTH_LONG).show()
-        }
+//        toggleViews(false)
+//        when (ex) {
+//            is ErrorConfig.NetworkException -> {
+//                Toast.makeText(this@MainActivity, ex.message, Toast.LENGTH_LONG).show()
+//            }
+//            is ErrorConfig.GitHubException -> {
+//                Toast.makeText(this@MainActivity, ex.message, Toast.LENGTH_LONG).show()
+//            }
+//            else -> Toast.makeText(this@MainActivity, "Oops! Something went wrong.", Toast.LENGTH_LONG).show()
+//        }
     }
 
     override fun onClick(v: View?) {
-        recyclerView.visibility = View.GONE
-        errorView.visibility = View.GONE
-        progressBar.visibility = View.VISIBLE
-        viewModel.searchUser(editText.text.toString())
+//        recyclerView.visibility = View.GONE
+//        errorView.visibility = View.GONE
+//        progressBar.visibility = View.VISIBLE
+//        viewModel.searchUser(editText.text.toString())
     }
 
     private fun toggleViews(dataAvailable: Boolean) {
-        progressBar.visibility = View.GONE
-        if (dataAvailable) {
-            errorView.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
-        } else {
-            recyclerView.visibility = View.GONE
-            errorView.visibility = View.VISIBLE
-        }
+//        progressBar.visibility = View.GONE
+//        if (dataAvailable) {
+//            errorView.visibility = View.GONE
+//            recyclerView.visibility = View.VISIBLE
+//        } else {
+//            recyclerView.visibility = View.GONE
+//            errorView.visibility = View.VISIBLE
+//        }
     }
 }
